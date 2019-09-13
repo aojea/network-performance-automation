26960dc38497d421429e1d4e393adc5c14202520
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-09-13 19:42:49 +0200

    First working tests

---
+++ labs/docker/README.md
@@ -0,0 +1,44 @@
+Install docker 
+
+Install kvm
+
+Install koko
+
+go install github.com/redhat-nfvpe/koko
+
+Launch trex
+
+docker run -it --rm --privileged --cap-add=ALL -v /mnt/huge:/mnt/huge -v /sys/bus/pci/devices:/sys/bus/pci/devices -v /sys/devices/system/node:/sys/devices/system/node -v /dev:/dev aojea/trex
+
+Create veth pair
+
+ ip link add veth0 type veth peer name veth1
+ ip addr add 10.1.0.1/24 dev veth0
+ ip addr add 10.1.0.2/24 dev veth1
+ ip link set veth0 up
+ ip link set veth1 up
+
+Configure Trex
+
+cat > /etc/trex_cfg.yaml <<EOF
+- port_limit    : 2
+  version       : 2
+  low_end       : true
+  interfaces    : ["veth0", "veth1"]   # list of the interfaces to bind run ./dpdk_nic_bind.py --status
+  port_info     :  # set eh mac addr
+
+                 - ip         : 1.1.1.1
+                   default_gw : 2.2.2.2
+                 - ip         : 2.2.2.2
+                   default_gw : 1.1.1.1
+EOF
+
+
+2.3. test commands
+Run TRex stateless using: “./t-rex-64 -i -c 1”
+
+In stateless console (“trex-console”) we do the following tests:
+var1: start -f stl/bench.py -t size=64,vm=var1 -m <rate> --port 0 --force
+cached: start -f stl/bench.py -t size=64,vm=cached -m <rate> --port 0 --force
+latency: start -f stl/udp_1pkt_src_ip_split_latency.py -m <rate> --port 0 --force
+
---
+++ labs/tests/test_simple.py
@@ -0,0 +1,47 @@
+import pytest
+import time
+from trex_stl_lib.api import *
+"""
+An example on how to use TRex for functional tests
+
+It can be used for various tasks and can replace simple Pagent/Scapy
+low rate tests
+
+TODO include the pyvcluster logic
+"""
+
+
+@pytest.mark.parametrize("protocol", ["TCP", "UDP", "ICMP"])
+def test_one_packet(trex, protocol):
+    tx_port, rx_port = trex.get_all_ports()
+    trex.reset(ports=[tx_port, rx_port])
+    # activate service mode on RX code
+    trex.set_service_mode(ports=rx_port)
+    # generate a simple UDP packet
+    pkt = Ether() / IP()
+    pkt[IP].src = "192.168.50.4"
+    pkt[IP].dst = "192.168.60.4"
+    if protocol == "TCP":
+        pkt = pkt / TCP()
+    elif protocol == "UDP":
+        pkt = pkt / UDP()
+    elif protocol == "ICMP":
+        pkt = pkt / ICMP()
+    # start a capture
+    capture = trex.start_capture(rx_ports=rx_port)
+    # push the UDP packet to TX port... we need 'force' because this is under service mode
+    print('\nSending 1 UDP packet(s) on port {}'.format(tx_port))
+    trex.push_packets(ports=tx_port, pkts=pkt, force=True)
+    trex.wait_on_traffic(ports=tx_port, rx_delay_ms=1000)
+    # we need to block with Virtualbox because the delay is huge
+    time.sleep(0.5)
+    rx_pkts = []
+    trex.stop_capture(capture_id=capture['id'], output=rx_pkts)
+    print('\nRecived {} packets on port {}:\n'.format(len(rx_pkts), rx_port))
+    trex.set_service_mode(ports=rx_port, enabled=False)
+
+    # got back one packet
+    assert(len(rx_pkts) == 1)
+    rx_scapy_pkt = Ether(rx_pkts[0]['binary'])
+    # Check if it's the same packet
+    assert(protocol in rx_scapy_pkt)
